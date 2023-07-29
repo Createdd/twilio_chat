1fb8c6a2205e877db6080679974c8f0de8dad5a2
danieldeutsch <deudan1010@gmail.com>
2023-07-29 10:44:09 +0200

    fix restructure

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 import json
 # Internal imports
-from models import Conversation, SessionLocal, get_customer_conversations, check_last_entry, is_number_in_database, update_status
+from models import Conversation, SessionLocal, get_customer_conversations, check_last_entry, is_number_in_database, \
+    update_status
 from utils import send_message, logger
 import datetime
 
@@ -40,6 +41,7 @@ openai.api_key = config("OPENAI_API_KEY")
 
 calendar_id = CALENDAR_ID2
 
+
 # Dependency
 def get_db():
     try:
@@ -58,16 +60,19 @@ class BookingData:
         self.status = None
         self.isotime = None
 
+
 booking_data = BookingData()
 
 
 @app.get("/")
 async def index():
-    return {"msg": "working"}\
-
+    return {"msg": "working"} \
+ \
+ \
 @app.get("/message")
 async def index():
     return {"msg": "Send a POST req"}
+
 
 def identify_booking_fields(chatgpt_response):
     try:
@@ -87,7 +92,9 @@ def identify_booking_fields(chatgpt_response):
 
     return booking_data.status
 
-def store_conversation_in_db(whatsapp_number, Body, chatgpt_response, date, time, name, now, status, db: Session = Depends(get_db)):
+
+def store_conversation_in_db(whatsapp_number, Body, chatgpt_response, date, time, name, now, status,
+                             db):
     try:
         conversation = Conversation(
             sender=whatsapp_number,
@@ -106,6 +113,7 @@ def store_conversation_in_db(whatsapp_number, Body, chatgpt_response, date, time
         db.rollback()
         logger.error(f"Error storing conversation in database: {e}")
 
+
 @app.post("/message")
 async def reply(request: Request, Body: str = Form(), db: Session = Depends(get_db)):
     # Extract the phone number from the incoming webhook request
@@ -114,18 +122,14 @@ async def reply(request: Request, Body: str = Form(), db: Session = Depends(get_
     whatsapp_number = form_data['From'].split("whatsapp:")[-1]
     print(f"Sending the ChatGPT response to this number: {whatsapp_number}")
 
-
-
-
     num_entries, message_content = get_customer_conversations(whatsapp_number)
     # print(num_entries)
 
-    if num_entries > 100:# todo set to 10
+    if num_entries > 100:  # todo set to 10
         send_message(whatsapp_number, '''
         Das Thema scheint kompliziert zu sein. Es gibt viele Fragen. 
 Wir leiten dich an einen Mitarbeiter weiter.''')
         return 'done'
-
 
     # Get or create the user session for the specific route
     # user_session = db.query(UserSession).filter_by(user_id=whatsapp_number, route_name="message").first()
@@ -148,7 +152,7 @@ Wir leiten dich an einen Mitarbeiter weiter.''')
     # )
     #
     # chatgpt_response = response.choices[0].message.content
-    chatgpt_response='''{  "name": "ZZZZZ",
+    chatgpt_response = '''{  "name": "ZZZZZ",
   "time": "23:00:00",
   "date": "2023-07-27",
   "isotime": "2023-07-27T23:00:00"
@@ -176,18 +180,18 @@ Wir leiten dich an einen Mitarbeiter weiter.''')
                                  booking_data.time,
                                  booking_data.name,
                                  now,
-                                 booking_data.status)
+                                 booking_data.status,
+                                 db)
 
         if booking_data.status == 'NO_TIME_OR_DATE':
             return f'Kannst du mir nochmal sagen, wann du du genau einen Termin möchtest und für welchen Namen ich diesen buchen darf? {e}'
         else:
-            available = get_google_calendar_availability(calendar_id, isotime)
+            available = get_google_calendar_availability(calendar_id, booking_data.isotime)
 
     if booking_data.available is None:
         # send_message(whatsapp_number, 'Wir leiten dich an einen Mitarbeiter weiter.')
         booking_data.status = 'NO_INFO_ON_AVAILABILITY'
         update_status(whatsapp_number, booking_data.status)
-
 
         return booking_data.status
 
@@ -211,7 +215,7 @@ Wir leiten dich an einen Mitarbeiter weiter.''')
     else:
         status = 'NOT_AVAILABLE'
         update_status(whatsapp_number, status)
-        response= f'Der Termin ist nicht mehr frei. Nenne einen anderen'
+        response = f'Der Termin ist nicht mehr frei. Nenne einen anderen'
         # send_message(whatsapp_number, 'Der Termin ist nicht mehr frei. Nenne einen anderen')
 
         return response
@@ -257,26 +261,23 @@ Wir leiten dich an einen Mitarbeiter weiter.''')
 #     return "done"
 
 
-    # try:
-    #     booking_request = json.loads(chatgpt_response)
-    #     isotime = booking_request['isotime']
-    #     # calendar_id = booking_request['calendar_id']
-    #     calendar_id = CALENDAR_ID2
-    #     name = booking_request['name']
-    #
-    #     # available = get_google_calendar_availability(calendar_id, isotime)
-    #
-    #     date = booking_request['date']
-    #     time = booking_request['time']
-    #     print('-----------------------PRINT worked ', available, date, time, name)
-    #
-    # except Exception as e:
-    #     send_message(whatsapp_number,
-    #                  f'Kannst du mir nochmal sagen, wann du du genau einen Termin möchtest und für welchen Namen ich diesen buchen darf? {e}')
-    #     status = 'NO_TIME_OR_DATE'
+# try:
+#     booking_request = json.loads(chatgpt_response)
+#     isotime = booking_request['isotime']
+#     # calendar_id = booking_request['calendar_id']
+#     calendar_id = CALENDAR_ID2
+#     name = booking_request['name']
+#
+#     # available = get_google_calendar_availability(calendar_id, isotime)
+#
+#     date = booking_request['date']
+#     time = booking_request['time']
+#     print('-----------------------PRINT worked ', available, date, time, name)
+#
+# except Exception as e:
+#     send_message(whatsapp_number,
+#                  f'Kannst du mir nochmal sagen, wann du du genau einen Termin möchtest und für welchen Namen ich diesen buchen darf? {e}')
+#     status = 'NO_TIME_OR_DATE'
 
 
-
-
-
-        # return status
+# return status
